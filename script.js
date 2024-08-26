64c8f393dcd5348e023ddd02f4e5766ba669e7a5
sardor-dev1 <sardorprogramist@gmail.com>
2024-08-26 11:52:05 +0500

    examples

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+// function countPositiveNumbers(arr: number[]): number {
+//     let count = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > 0) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// const numbers = [-3, 5, 0, 2, -8, 1];
+// console.log(countPositiveNumbers(numbers));
+//task-2
+// function fizzBuzz(n: number): void {
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+// fizzBuzz(20);
